bb6b13f93c4c1391d7480d5ec23f2254bfd4249c
ananyateklu <teklu009@morris.umn.edu>
2019-11-24 13:07:06 -0600

    Create the function disemvowel that takes a word and returns the word without the vowels
    All the tests pass

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,21 @@ fn main() {
     println!("s was '{}', and without vowels is '{}'.", s, s_disemvowel);
 }
 
+fn disemvowel(word: &str) -> String {
+    let mut newword = String::new(); 
+    //iterate through the word that is being passed in
+    for i in word.chars() {
+        //check if the word has any vowels
+        if i.to_string() != "a" && i.to_string() != "e" && i.to_string() != "i" && i.to_string() != "o" && i.to_string() != "u" && i.to_string() != "A" && i.to_string() != "E" && i.to_string() != "I" && i.to_string() != "O" && i.to_string() != "U"{
+            //put all the letters that are not vowels into the variable newword
+            newword.push(i);
+        }
+
+    }
+    //return the word with no vowels 
+    newword
+}
+
 // Everything from here down is Rust test code. You shouldn't need to 
 // change any of this. 
 //
@@ -13,6 +28,7 @@ fn main() {
 // fail because there's no definition for the `disemvowel` function. Add
 // that up above and work to get the tests to pass. See the lab write-up
 // for some tips.
+
 
 #[cfg(test)]
 mod tests {
@@ -67,3 +83,4 @@ mod tests {
             disemvowel("We can handle emoji as well! 🤣😃👍"))
     }
 }
+
